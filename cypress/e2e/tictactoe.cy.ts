dfd79d715247758ef6ee7ae21a7f207a979f5823
Haley Bowler <haleybowler@Haleys-MacBook.local>
2024-03-02 14:53:56 -0500

    first commit

---
+++ cypress/e2e/tictactoe.cy.ts
@@ -0,0 +1,85 @@
+/// <reference types="cypress" />
+
+describe("Tic Tac Toe App", () => {
+    beforeEach(() => {
+        cy.visit("https://roomy-fire-houseboat.glitch.me/")
+    })
+
+    function createBoard(size) {
+        cy.get("#number").type(size.toString())
+        cy.get("#start").click()
+    }
+
+    function validateBoard(size) {
+        cy.get("#table td").its("length").should("eq", size * size)
+        cy.get("#table tr").its("length").should("eq", size)
+    }
+
+    it("Displays an input form with a play button", () => {
+        cy.get("#number").should("exist")
+        cy.get("#start").should("exist")
+        cy.get("#table").contains("td").should("not.exist")
+    })
+
+    it("Accepts 3 as a valid input", () => {
+        createBoard(3)
+        validateBoard(3)
+    })
+
+    it("Accepts 9 as a valid input", () => {
+        createBoard(9)
+        validateBoard(9)
+    })
+
+    it("Accepts 90 as a valid input", () => {
+        createBoard(90)
+        validateBoard(90)
+    })
+
+    it("Does not accept hello as a valid input", () => {
+        createBoard("hello")
+        cy.get("#table").contains("td").should("not.exist")
+    })
+
+    it("Follows X -> O order", () => {
+        createBoard(3)
+        cy.get("#table td").eq(0).click()
+        cy.get("#table td").eq(0).should("have.text", "X")
+        cy.get("#table td").eq(1).click()
+        cy.get("#table td").eq(1).should("have.text", "O")
+    })
+
+    it("Obeys simple win condition", () => {
+        createBoard(3)
+        // X O -
+        // O X -
+        // - - X
+        cy.get("#table td").eq(0).click()
+        cy.get("#table td").eq(1).click()
+        cy.get("#table td").eq(4).click()
+        cy.get("#table td").eq(3).click()
+        cy.get("#table td").eq(8).click()
+        // Known failure: end game prompt congratulates wrong player
+        cy.get("#endgame").should("include.text", "player X")
+    })
+
+    it("Obeys tie condition", () => {
+        createBoard(3)
+        // TBD: should game end as soon as tie is inevitable, or once board is full
+        // O X O
+        // X X O
+        // X O X
+        cy.get("#table td").eq(4).click()
+        cy.get("#table td").eq(2).click()
+        cy.get("#table td").eq(8).click()
+        cy.get("#table td").eq(0).click()
+        cy.get("#table td").eq(3).click()
+        cy.get("#table td").eq(5).click()
+        cy.get("#table td").eq(6).click()
+        cy.get("#table td").eq(7).click()
+        cy.get("#table td").eq(1).click()
+        // Known failure: game does not recognize tie condition
+        cy.get("#endgame").should("include.text", "tie game")
+    })
+    
+})
